5d7844f2176446937baaba5139e85c9f196527f8
Geno Ferollo <genemators@gmail.com>
2020-12-09 04:12:26 +0500

    std

--- components/Programs/Student.tsx
+++ components/Programs/Student.tsx
@@ -2,7 +2,7 @@ import styles from '@/styles/Programs/Blog.module.scss';
 
 import type { AppComponent } from '@/types/utils/programs';
 
-const STUDENT_HOME_PAGE = 'https://intranet.wiut.uz/';
+const STUDENT_HOME_PAGE = 'https://intranet.wiut.uz/Account/Login?ReturnUrl=%2fUserModuleMaterials';
 
 const Student: React.FC<AppComponent> = () => (
   <iframe className={styles.blog} title="Student" src={STUDENT_HOME_PAGE} />
