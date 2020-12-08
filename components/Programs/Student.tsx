7b718ad779790eee4bcc815e3bb1c13d9a5c2c5b
Geno Ferollo <genemators@gmail.com>
2020-12-09 04:15:25 +0500

    Update Student.tsx

--- components/Programs/Student.tsx
+++ components/Programs/Student.tsx
@@ -2,7 +2,7 @@ import styles from '@/styles/Programs/Blog.module.scss';
 
 import type { AppComponent } from '@/types/utils/programs';
 
-const STUDENT_HOME_PAGE = 'https://intranet.wiut.uz/';
+const STUDENT_HOME_PAGE = 'https://student.genemator.me/';
 
 const Student: React.FC<AppComponent> = () => (
   <iframe className={styles.blog} title="Student" src={STUDENT_HOME_PAGE} />
