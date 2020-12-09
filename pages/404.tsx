d78997c24edd23bd71f38a56ce7c362d6e9cd232
Geno Ferollo <genemators@gmail.com>
2020-12-09 17:20:25 +0500

    error fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,6 @@
 import type { ReactElement } from 'react';
 import { Head } from 'next/document';
-// @ts-ignore
-import styles from '@/styles/error.scss';
-
+import Error from '@/components/System/Error';
 
 export default function ErrorPage(): ReactElement {
   return (
@@ -14,19 +12,8 @@ export default function ErrorPage(): ReactElement {
           property="og:description"
           content="This page's preview is not available as this page doesn't exist"
         />
-        <style>
-
-        </style>
       </Head>
-      <div className={styles.container && styles.bsod}>
-        <h1 className={styles.neg && styles.title}><span className={styles.bg}>Error - 404</span></h1>
-        <p>An error has occurred, to continue:</p>
-        <p>* Return to our homepage.<br />
-          * Send us a feedback about this error and try later.</p>
-        <nav className={styles.nav}>
-          <a href="/" className={styles.link}>index</a>&nbsp;|&nbsp;<a href="https://genemator.me" className={styles.link}>webmaster</a>
-        </nav>
-      </div>
+      <Error/>
     </>
   );
 }
