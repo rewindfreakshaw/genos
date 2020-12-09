1e44fb483340abec9acefc014a1aba025c091b72
Geno Ferollo <genemators@gmail.com>
2020-12-09 17:04:09 +0500

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -19,11 +19,11 @@ export default function ErrorPage(): ReactElement {
       </Head>
       <div className="bsod container">
         <h1 className="neg title"><span className="bg">Error - 404</span></h1>
-        <p>An error has occured, to continue:</p>
+        <p>An error has occurred, to continue:</p>
         <p>* Return to our homepage.<br />
           * Send us an e-mail about this error and try later.</p>
         <nav className="nav">
-          <a href="#" className="link">index</a>&nbsp;|&nbsp;<a href="#" className="link">webmaster</a>
+          <a href="/" className="link">index</a>&nbsp;|&nbsp;<a href="https://genemator.me" className="link">webmaster</a>
         </nav>
       </div>
     </>
