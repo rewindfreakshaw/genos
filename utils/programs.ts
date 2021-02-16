371b74d649cadda89abe8f650d51278dfa68081c
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-02-16 19:55:10 +0500

    removed students

--- utils/programs.ts
+++ utils/programs.ts
@@ -5,7 +5,6 @@ import { extname } from 'path';
 import { isValidUrl } from '@/utils/url';
 import { loaderOptions as blogLoaderOptions } from '@/components/Programs/Blog';
 import { loaderOptions as genematorLoaderOptions } from '@/components/Programs/Genemator';
-import { loaderOptions as studentLoaderOptions } from '@/components/Programs/Student';
 import { loaderOptions as dosLoaderOptions } from '@/components/Programs/Dos';
 import { loaderOptions as explorerLoaderOptions } from '@/components/Programs/Explorer';
 import { loaderOptions as webampLoaderOptions } from '@/components/Programs/WebODF';
@@ -14,7 +13,6 @@ import { ROOT_DIRECTORY } from '@/utils/constants';
 
 const Blog = dynamic(import('@/components/Programs/Blog'));
 const Genemator = dynamic(import('@/components/Programs/Genemator'));
-const Student = dynamic(import('@/components/Programs/Student'));
 const Dos = dynamic(import('@/components/Programs/Dos'));
 const Explorer = dynamic(import('@/components/Programs/Explorer'));
 const WebODF = dynamic(import('@/components/Programs/WebODF'));
@@ -28,10 +26,6 @@ const appLoaders: AppLoaders = {
   genemator: {
     loader: Genemator,
     loaderOptions: genematorLoaderOptions
-  },
-  student: {
-    loader: Student,
-    loaderOptions: studentLoaderOptions
   },
   dos: {
     loader: Dos,
