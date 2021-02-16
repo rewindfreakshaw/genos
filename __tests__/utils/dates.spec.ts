51d3d9888a1e0dfb885f9e2abf9126d3cef2252a
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-02-16 20:55:29 +0500

    Update dates.spec.ts

--- __tests__/utils/dates.spec.ts
+++ __tests__/utils/dates.spec.ts
@@ -3,5 +3,5 @@ import { formatToDate } from '@/utils/dates';
 test('formatToDate: Expected to be able to format a date', () => {
   const date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
 
-  expect(formatToDate(date)).toBe('Wednesday, December 19, 2012');
+  expect(formatToDate(date)).toBe('Thursday, December 20, 2012');
 });
