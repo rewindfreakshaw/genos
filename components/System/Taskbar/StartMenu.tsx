5dc2ffca21e6ad2ea141804d465489ac9be5db7d
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-02-16 20:06:06 +0500

    Update StartMenu.tsx

--- components/System/Taskbar/StartMenu.tsx
+++ components/System/Taskbar/StartMenu.tsx
@@ -4,7 +4,7 @@ import ButtonBar from '@/components/System/Taskbar/ButtonBar';
 import FileManager from '@/components/System/FileManager/FileManager';
 import MenuView from '@/components/System/FileManager/MenuView';
 import { createPortal } from 'react-dom';
-import { faWindows } from '@fortawesome/free-brands-svg-icons';
+import { faApple } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { getNextContainerElement } from '@/utils/elements';
 import { useEffect, useRef, useState } from 'react';
