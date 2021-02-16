d65d6320da38ed3f3718df3c406fd34fd49a09c0
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-02-16 19:25:39 +0500

    Update Blog.tsx

--- components/Programs/Blog.tsx
+++ components/Programs/Blog.tsx
@@ -2,7 +2,7 @@ import styles from '@/styles/Programs/Blog.module.scss';
 
 import type { AppComponent } from '@/types/utils/programs';
 
-const BLOG_HOME_PAGE = 'https://genemator.me/';
+const BLOG_HOME_PAGE = 'https://old.bsba.uz/';
 
 const Blog: React.FC<AppComponent> = () => (
   <iframe className={styles.blog} title="Blog" src={BLOG_HOME_PAGE} />
