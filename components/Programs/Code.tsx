7bed3ce2f49966a1d016490b05e507317421a0f2
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-03-30 22:22:26 +0500

    Update Code.tsx

--- components/Programs/Code.tsx
+++ components/Programs/Code.tsx
@@ -1,25 +1,61 @@
 // import styles from '@/styles/Programs/Code.module.scss';
 import type { AppComponent } from '@/types/utils/programs';
+import React, {useState} from 'react';
+import {auto} from "framer-motion/types/render/dom/utils/value-types";
 
-let AUTO_STATE_WINDOW = false
 const CODE_HOME_PAGE = 'https://genemator-code.herokuapp.com';
-const OPEN_THE_WINDOW = () => {
-  window.open(CODE_HOME_PAGE, '', "toolbar=yes,scrollbars=yes,resizable=yes,height=850,width=1200")
-}
 
 const Code: React.FC<AppComponent> = () => {
-  if (!AUTO_STATE_WINDOW) {
-    OPEN_THE_WINDOW()
-    AUTO_STATE_WINDOW = true
+  let codeWindow: Window | null;
+  const [autoOpen, setAutoOpen] = useState(false)
+
+  if (!autoOpen) {
+    setAutoOpen(true)
+    codeWindow = window.open(
+      CODE_HOME_PAGE,
+      'codeWindow',
+      'width=1200, height=900'
+    );
   }
 
   return (
-    <div>
-      <h2>Opening in a new global tab!</h2><br/>
-    </div>
+    <article>
+      <div style={{
+        textAlign: "center",
+        justifyContent: "center",
+      }}>
+        <h2>
+          In order to let vscode keymaps work correctly, we opened it in a new
+          global window!
+        </h2>
+        <br />
+        <h3>If it didn&apos;t appear, press &quot;spawn the window&quot; button below:</h3>
+        <button
+          type="button"
+          style={{
+            margin: 10,
+            padding: 10,
+            background: "black",
+            color: "white",
+            border: "none",
+            borderRadius: 5
+          }}
+          onClick={() => {
+            codeWindow = window.open(
+              CODE_HOME_PAGE,
+              'codeWindow',
+              'width=900, height=1200'
+            );
+          }}
+        >
+          Spawn the window
+        </button>
+      </div>
+    </article>
 
-  // <iframe className={styles.code} title="Genium's Code" src={CODE_HOME_PAGE} />
-)};
+    // <iframe className={styles.code} title="Genium's Code" src={CODE_HOME_PAGE} />
+  );
+};
 
 export default Code;
 
