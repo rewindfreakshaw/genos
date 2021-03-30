74134d64ba43c3acec0cb2aca542243cbf5f83b5
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-03-30 22:31:47 +0500

    Update Code.tsx

--- components/Programs/Code.tsx
+++ components/Programs/Code.tsx
@@ -1,50 +1,59 @@
 // import styles from '@/styles/Programs/Code.module.scss';
 import type { AppComponent } from '@/types/utils/programs';
-import React, {useState} from 'react';
-import {auto} from "framer-motion/types/render/dom/utils/value-types";
+import React, { useState } from 'react';
 
 const CODE_HOME_PAGE = 'https://genemator-code.herokuapp.com';
 
 const Code: React.FC<AppComponent> = () => {
   let codeWindow: Window | null;
-  const [autoOpen, setAutoOpen] = useState(false)
+  const [autoOpen, setAutoOpen] = useState(false);
+  const windowSettings: { target: string; features: string } = {
+    target: 'codeWindow',
+    features:
+      'width=1200,height=900,toolbar=no,menubar=no,resizable=yes'
+  };
 
   if (!autoOpen) {
-    setAutoOpen(true)
+    setAutoOpen(true);
     codeWindow = window.open(
       CODE_HOME_PAGE,
-      'codeWindow',
-      'width=1200, height=900'
+      windowSettings.target,
+      windowSettings.features
     );
   }
 
   return (
     <article>
-      <div style={{
-        textAlign: "center",
-        justifyContent: "center",
-      }}>
+      <div
+        style={{
+          textAlign: 'center',
+          justifyContent: 'center'
+        }}
+      >
         <h2>
           In order to let vscode keymaps work correctly, we opened it in a new
           global window!
         </h2>
         <br />
-        <h3>If it didn&apos;t appear, press &quot;spawn the window&quot; button below:</h3>
+        <h3>
+          If it didn&apos;t appear, press &quot;spawn the window&quot; button
+          below:
+        </h3>
         <button
           type="button"
           style={{
             margin: 10,
             padding: 10,
-            background: "black",
-            color: "white",
-            border: "none",
+            background: 'black',
+            color: 'white',
+            border: 'none',
             borderRadius: 5
           }}
           onClick={() => {
             codeWindow = window.open(
               CODE_HOME_PAGE,
-              'codeWindow',
-              'width=900, height=1200'
+              windowSettings.target,
+              windowSettings.features
             );
           }}
         >
